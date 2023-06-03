fffdb1c29d1c98552dfb8cc2a20100a45527b025
xihan123 <srxqzxs@vip.qq.com>
2023-06-04 03:23:24 +0800

    init

--- script.js
+++ script.js
@@ -3,27 +3,8 @@
     let firstSquish = true;
 
     const LANGUAGES = {
-        "_": { defaultLanguage: "en", defaultVOLanguage: "ja" },
-        "en": {
-            audioList: null,
-            texts: {
-                "page-title": "Welcome to herta kuru~",
-                "doc-title": "Kuru Kuru~",
-                "page-descriptions": "The website for Herta, the <del>annoying</del> cutest genius Honkai: Star Rail character out there.",
-                "counter-descriptions": ["The kuru~ has been squished for", "Herta has been kuru~ed for"],
-                "counter-unit": "times",
-                "counter-button": ["Squish the kuru~!", "Kuru kuru~!"],
-                "access-via-pages": "You're currently accessing via GitHub Pages. For users in China (Mainland) or some other regions, click <a href='https://herta.ft2.ltd/'>here to access the mirror on Netlify</a>. ",
-                "access-via-mirror": "Congratulations! You are using a mirror site, which should speed up access within China (Mainland) and some regions. Click here to <a href='https://duiqt.github.io/herta_kuru/'>visit the source site on GitHub Pages</a>.",
-                "show-credits-text": "Show Credits",
-                "repository-desc": "GitHub Repo",
-                "options-txt-vo-lang": "Voice-Over Language",
-                "options-txt-lang": "Page Language",
-                "dialogs-close": "Close",
-                "dialogs-credits-title": "Credits"
-            },
-            cardImage: "img/card_en.jpg"
-        }, "cn": {
+        "_": { defaultLanguage: "cn", defaultVOLanguage: "cn" },
+		"cn": {
             audioList: [
                 // TODO audio random weight
                 "audio/cn/gululu.mp3",
@@ -33,14 +14,14 @@
                 "audio/cn/要坏掉了.mp3"
             ],
             texts: {
-                "page-title": "黑塔转圈圈",
+                "page-title": "尼奈转圈圈",
                 "doc-title": "咕噜噜~",
-                "page-descriptions": "给黑塔酱写的小网站，对，就是那个<del>烦人的</del>最可爱的《崩坏：星穹铁道》角色！",
-                "counter-descriptions": ["黑塔已经咕噜噜~了", "黑塔已经转了"],
+                "page-descriptions": "给尼奈酱写的小网站，对，就是那个<del>烦人的</del>最可爱的B站虚拟UP主！",
+                "counter-descriptions": ["尼奈已经咕噜噜~了", "尼奈已经转了"],
                 "counter-unit": ["次", "次圈圈"],
                 "counter-button": ["转圈圈~", "咕噜噜！"],
-                "access-via-pages": "您目前是通过 GitHub Pages 访问。对于中国大陆或其他一些地区的用户，请<a href='https://herta.ft2.ltd/'>单击此处访问 Netlify 上的镜像</a>。",
-                "access-via-mirror": "恭喜！你正在使用镜像站，这应当会加速在中国大陆及部分地区境内的访问。点此<a href='https://duiqt.github.io/herta_kuru/'>访问 GitHub Pages 上的源站</a>。",
+                "access-via-pages": "您目前是通过 GitHub Pages 访问。对于中国大陆或其他一些地区的用户，请<a href='https://herta.ft2.ltd/'>单击此处访问 国内服务器 上的镜像</a>。",
+                "access-via-mirror": "恭喜！你正在使用镜像站，这应当会加速在中国大陆及部分地区境内的访问。点此<a href='https://duiqt.github.io/nine_kuru/'>访问 GitHub Pages 上的源站</a>。",
                 "show-credits-text": "查看感谢页",
                 "repository-desc": "GitHub 仓库",
                 "options-txt-vo-lang": "语音语言",
@@ -57,9 +38,9 @@
                 "audio/ja/kuru2.mp3",
             ],
             texts: {
-                "page-title": "ヘルタクルへようこそ~",
+                "page-title": "ようこそ、ニナイへ~",
                 "doc-title": "クル クル~",
-                "page-descriptions": "このサイトはヘルタのために作られた、 あの崩壊：スターレイルの <del>悩ましい</del> かわいい天才キャラー。",
+                "page-descriptions": "このサイトは、ナインのために作られました、 あの BiliBili VTB <del>悩ましい</del> かわいい米粒の虫",
                 "counter-descriptions": "全世界のクル再生数",
                 "counter-unit": "回",
                 "counter-button": "クル クル~!",
@@ -74,46 +55,6 @@
             },
             cardImage: "img/card_ja.jpg"
         },
-        "kr": {
-            audioList: null,
-            texts: {
-                "page-title": "헤르타빙글 환영합니다~",
-                "doc-title": "빙글 빙글~",
-                "page-descriptions": "이 웹사이트는 헤르타를 위해 만들어졌습니다, 붕괴: 스타레일 의 <del>귀찮은</del> 귀여운 천재 ",
-                "counter-descriptions": "전 세계 빙글 조회수",
-                "counter-unit": "번",
-                "counter-button": "빙글 빙글~!",
-                "access-via-pages": "당신은 GitHub를 통해 액세스하고 있습니다. 중국 또는 기타 지역 사용자의 경우 여기를 <a href='https://herta.ft2.ltd/'>클릭하여 Netlify의 미러 액세스하십시오</a>. ",
-                "access-via-mirror": "축하합니다. 당신은 미러 웹 사이트를 통해 액세스하고 있습니다, 이렇게 하면 중국 및 일부 다른 지역에서의 액세스 속도가 빨라집니다. 여기를 <a href='https://duiqt.github.io/herta_kuru/'> 클릭하여 GitHub의 소스 페이지에 액세스하십시오</a>. ",
-                "show-credits-text": "Show Credits",
-                "repository-desc": "GitHub Repo",
-                "options-txt-vo-lang": "Voice-Over Language",
-                "options-txt-lang": "Page Language",
-                "dialogs-close": "Close",
-                "dialogs-credits-title": "Credits"
-            },
-            cardImage: "img/card_kr.jpg"
-        },
-        "id": {
-            audioList: null,
-            texts: {
-                "page-title": "Selamat datang di Herta kuru~",
-                "doc-title": "Kuru Kuru~",
-                "page-descriptions": "Situs web yang dipersembahkan kepada Herta, sang Karakter Jenius <del>ngeselin</del> dari Honkai: Star Rail.",
-                "counter-descriptions": ["Kuru nya telah dipencet sebanyak", "Herta telah ter-kuru-kan sebanyak"],
-                "counter-unit": "kali",
-                "counter-button": ["Pencet kuru nya~!", "Kuru kuru~!"],
-                "access-via-pages": "Kamu saat ini sedang mengakses melalui Laman GitHub. Untuk pengguna berdomisili di Tiongkok (Daratan) dan wilayah lainnya, silakan klik <a href='https://herta.ft2.ltd/'> di sini untuk mengakses mirror nya di Netlify</a>. ",
-                "access-via-mirror": "Selamat! Kamu sedang menggunakan situs mirror, yang mana seharusnya mempercepat akses untuk wilayah sekitar Tiongkok (daratan) dan wilayah lainnya . Silakan klik <a href='https://duiqt.github.io/herta_kuru/'> di sini untuk mengunjungi situs sumbernya pada Laman Github</a>.",
-                "show-credits-text": "Tampilkan Credit",
-                "repository-desc": "GitHub Repo",
-                "options-txt-vo-lang": "Voice-Over Language",
-                "options-txt-lang": "Page Language",
-                "dialogs-close": "Close",
-                "dialogs-credits-title": "Credits"
-            },
-            cardImage: "img/card_id.jpg"
-        }
     };
 
     // This code tries to retrieve the saved language 'lang' from localStorage. If it is not found or if its value is null, then it defaults to "en". 
@@ -124,7 +65,7 @@
     function getLocalText(textId, language = null, fallback = true) {
         let curLang = LANGUAGES[language || current_language];
         let localTexts = curLang.texts;
-        if (localTexts[textId] != undefined) {
+        if (localTexts[textId] !== undefined) {
             let value = localTexts[textId];
             if (value instanceof Array) {
                 return randomChoice(value); // if there are multiple translations available for this text id, it randomly selects one of them and returns it.
@@ -147,7 +88,7 @@
         });
         refreshDynamicTexts()
         document.getElementById("herta-card").src = curLang.cardImage; // sets the image of element with id "herta-card" to the translated version in the selected language.
-    };
+    }
 
     multiLangMutation() // the function multiLangMutation is called initially when the page loads.
 
@@ -208,8 +149,8 @@
     };
 
     // try caching the hertaa1.gif and hertaa2.gif images by calling the tryCacheUrl function
-    tryCacheUrl("img/hertaa1.gif");
-    tryCacheUrl("img/hertaa2.gif");
+    tryCacheUrl("img/nine1.gif");
+    //tryCacheUrl("img/hertaa2.gif");
 
     // Define a function that takes an array as an argument and returns a random item from the array
     function randomChoice(myArr) {
@@ -255,9 +196,9 @@
 
     function animateHerta() {
         let id = null;
-        const random = Math.floor(Math.random() * 2) + 1;
+        const random = Math.floor(Math.random() * 1) + 1;
         const elem = document.createElement("img");
-        elem.src = tryCacheUrl(`img/hertaa${random}.gif`);
+        elem.src = tryCacheUrl(`img/nine${random}.gif`);
         elem.style.position = "absolute";
         elem.style.right = "-500px";
         elem.style.top = counterButton.getClientRects()[0].bottom + scrollY - 430 + "px"
@@ -309,16 +250,16 @@
                 if (document.getElementById(textId) != undefined)
                     document.getElementById(textId).innerHTML = randomChoice(value);
         });
-    };
-
-    // This block dynamically displays different messages depending on which hostname the website is being loaded from.
-    if (location.hostname == "herta.ft2.ltd" || location.hostname == "hertakuru.netlify.app") {
-        document.getElementById("access-via-tip-parent").innerHTML = "<p id='access-via-mirror'>Congratulations! You are using a mirror site, which should speed up access within China (Mainland) and some regions. Click here to <a href='https://duiqt.github.io/herta_kuru/'>visit the source site on GitHub Pages</a>.</p>";
-        multiLangMutation();
-    } else {
-        document.getElementById("access-via-tip-parent").innerHTML = "<p id='access-via-pages'>You're currently accessing via GitHub Pages. For users in China (Mainland) or some regions, click <a href='https://duiqt.github.io/herta_kuru/'>here to access the mirror on Netlify</a>.</p>";
-        multiLangMutation();
     }
+
+    // // This block dynamically displays different messages depending on which hostname the website is being loaded from.
+    // if (location.hostname == "herta.ft2.ltd" || location.hostname == "hertakuru.netlify.app") {
+    //     document.getElementById("access-via-tip-parent").innerHTML = "<p id='access-via-mirror'>Congratulations! You are using a mirror site, which should speed up access within China (Mainland) and some regions. Click here to <a href='https://duiqt.github.io/herta_kuru/'>visit the source site on GitHub Pages</a>.</p>";
+    //     multiLangMutation();
+    // } else {
+    //     document.getElementById("access-via-tip-parent").innerHTML = "<p id='access-via-pages'>You're currently accessing via GitHub Pages. For users in China (Mainland) or some regions, click <a href='https://duiqt.github.io/herta_kuru/'>here to access the mirror on Netlify</a>.</p>";
+    //     multiLangMutation();
+    // }
 
     // This function fetches data stored in a JSON file and displays it in a dialog box.
     function showCredits() {
@@ -330,7 +271,7 @@
             for (let i = 0; i < contributors.length; i++) {
                 var current = contributors[i];
                 let renderedName = current.username;
-                if (current.name != undefined) {
+                if (current.name !== undefined) {
                     renderedName += " (" + current.name + ")";
                 }
                 creditsHtmlContent += `<li class="mdui-list-item mdui-ripple">
@@ -365,18 +306,15 @@
     function showOptions() {
         mdui.dialog({
             title: 'Options',
-            content: `<div style="min-height: 350px;" class="mdui-typo">
+            content: `<div class="mdui-typo" style="min-height: 350px;">
     <label id="options-txt-lang">Page Language</label>
-    <select id="language-selector" class="mdui-select" mdui-select='{"position": "bottom"}'>
-        <option value="en">English</option>
+    <select class="mdui-select" id="language-selector" mdui-select='{"position": "bottom"}'>
         <option value="cn">中文</option>
         <option value="ja">日本語</option>
-        <option value="kr">한국어</option>
-        <option value="id">Bahasa Indonesia</option>
     </select>
     <br />
     <label id="options-txt-vo-lang">Voice-Over Language</label>
-    <select id="vo-language-selector" class="mdui-select" mdui-select='{"position": "bottom"}'>
+    <select class="mdui-select" id="vo-language-selector" mdui-select='{"position": "bottom"}'>
         <option value="ja">日本語</option>
         <option value="cn">中文</option>
     </select>
